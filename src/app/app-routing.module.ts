d5070a738d6845bd928e39d3d6363b307b552e26
Karol <karol-pyszka@users.noreply.github.com>
2023-03-15 09:24:53 +0100

    add 404 page and user component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+import { HomePageComponent } from './user/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent},
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CookieService } from 'ngx-cookie-service';
+import { HomePageComponent } from './user/home-page/home-page.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
