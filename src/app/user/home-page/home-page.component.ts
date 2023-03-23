0ff3a2597c5e8a6775e0d8ac93f0b4955d1794e7
karol-pyszka <karol200028@gmail.com>
2023-03-23 11:57:23 +0100

    add dialogs for edition and delete

--- src/app/user/home-page/home-page.component.ts
+++ src/app/user/home-page/home-page.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import {MatTableDataSource} from '@angular/material/table';
+import { Router } from '@angular/router';
 
 export interface ProjectElement {
   position: number;
@@ -11,12 +13,12 @@ export interface ProjectElement {
 
 const ELEMENT_DATA: ProjectElement[] = [
   {position: 1, name: 'Projekt testowy', description: 'opis projektu', createdAt: '2020-04-25', endTo: '2020-04-26' },
-  {position: 2, name: 'Projekt testowy', description: 'opis projektu', createdAt: '2020-04-25', endTo: '2020-04-26' },
-  {position: 3, name: 'Projekt testowy', description: 'opis projektu', createdAt: '2020-04-25', endTo: '2020-04-26' },
-  {position: 4, name: 'Projekt testowy', description: 'opis projektu', createdAt: '2020-04-25', endTo: '2020-04-26' },
-  {position: 5, name: 'Projekt testowy', description: 'opis projektu', createdAt: '2020-04-25', endTo: '2020-04-26' },
-  {position: 6, name: 'Projekt testowy', description: 'opis projektu', createdAt: '2020-04-25', endTo: '2020-04-26' },
-  {position: 7, name: 'Projekt testowy', description: 'opis projektu', createdAt: '2020-04-25', endTo: '2020-04-26' },
+  {position: 2, name: 'Projekt testowy2', description: 'opis projektu', createdAt: '2020-04-25', endTo: '2020-04-26' },
+  {position: 3, name: 'Projekt testowy3', description: 'opis projektu', createdAt: '2020-04-25', endTo: '2020-04-26' },
+  {position: 4, name: 'Projekt testowy4', description: 'opis projektu', createdAt: '2020-04-25', endTo: '2020-04-26' },
+  {position: 5, name: 'Projekt testowy5', description: 'opis projektu', createdAt: '2020-04-25', endTo: '2020-04-26' },
+  {position: 6, name: 'Projekt testowy6', description: 'opis projektu', createdAt: '2020-04-25', endTo: '2020-04-26' },
+  {position: 7, name: 'Projekt testowy7', description: 'opis projektu', createdAt: '2020-04-25', endTo: '2020-04-26' },
 ];
 
 @Component({
@@ -25,14 +27,23 @@ const ELEMENT_DATA: ProjectElement[] = [
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  
+  @ViewChild('deleteElementDialog')
+  deleteElementDialog!: TemplateRef<any>;
 
-  constructor() { }
+  @ViewChild('editElementDialog')
+  editElementDialog!: TemplateRef<any>;
+
+  constructor(public dialog: MatDialog, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   displayedColumns: string[] = ['position', 'name', 'description', 'createdAt', 'endTo', 'showTask','edit', 'delete'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
+  selectedProjectName = ''
+  projectDetails!: ProjectElement;
+
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -40,15 +51,34 @@ export class HomePageComponent implements OnInit {
   }
 
   showTask() {
-
+    this.router.navigate(["tasks"])
   }
 
-  editProject() {
-
+  editProject(data: any) {
+    this.projectDetails = data
+    let dialogRef = this.dialog.open(this.editElementDialog);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result)
+      if (result !== undefined) {
+          if (result === true) {
+            console.log(this.projectDetails)
+          }
+      }
+  })
   }
 
-  deleteProject() {
-    
+  deleteProject(data: any) {
+    this.selectedProjectName = data.name
+    let dialogRef = this.dialog.open(this.deleteElementDialog);
+    dialogRef.afterClosed().subscribe(result => {
+      if (result !== undefined) {
+          if (result === 'yes') {
+              console.log('User clicked yes.');
+          } else if (result === 'no') {
+              console.log('User clicked no.');
+          }
+      }
+  })
   }
 
 }
