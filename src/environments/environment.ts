a117d2b430366eba5ee77b22920147739b2d2795
karol-pyszka <karol200028@gmail.com>
2023-06-21 11:57:16 +0200

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,10 +7,6 @@ export const environment = {
   api: 'http://localhost:8080/api/'
 };
 
-export const env = {
-  
-}
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
